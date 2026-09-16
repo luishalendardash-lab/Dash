2a6046bd37e35b00b141f8011e6eae23e80cee85
luishalendardash-lab <luishalendardash@gmail.com>
2026-09-16 09:28:17 -0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -3558,7 +3558,7 @@ export default {
             supabase_url: !!env.SUPABASE_URL,
             supabase_key: !!env.SUPABASE_SERVICE_KEY,
             anon_key: !!env.SUPABASE_ANON_KEY,
-            versao: 'v89-erros-webhook',
+            versao: 'v90-periodo-ads',
             webhook_secret: env.WEBHOOK_SECRET ? `${env.WEBHOOK_SECRET.length} chars` : false,
             debug_token: !!env.DEBUG_TOKEN,
             lancamento_padrao: env.LANCAMENTO_PADRAO || false,
